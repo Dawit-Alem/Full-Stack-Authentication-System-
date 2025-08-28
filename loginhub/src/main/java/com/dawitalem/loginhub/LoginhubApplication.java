d4ebd24d7d6bc6c1e948bd8e74f5afd55d9d6e3a
Dawit Alem <dawitt.alem@gmail.com>
2025-08-28 18:37:51 -0400

    Initial commit: SpringBoot + React Vite projects

---
+++ loginhub/src/main/java/com/dawitalem/loginhub/LoginhubApplication.java
@@ -0,0 +1,13 @@
+package com.dawitalem.loginhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginhubApplication.class, args);
+	}
+
+}
---
+++ loginhub/src/test/java/com/dawitalem/loginhub/LoginhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawitalem.loginhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
